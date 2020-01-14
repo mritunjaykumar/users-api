6597f1d8562c471b2c0f09382e1662f46820a9e4
Mritunjay Kumar <jay.kumar@netapp.com>
2020-01-13 21:50:55 -0800

    added middleware

--- app/application.go
+++ app/application.go
@@ -2,29 +2,36 @@ package app
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mritunjaykumar/users-api/logger"
+	ginprometheus "github.com/zsais/go-gin-prometheus"
 )
 
 var (
 	router = gin.Default()
 )
 
-func DummyMiddleware(c *gin.Context) {
-	fmt.Println("Im a dummy!")
+// LatencyMiddleware captures api latency
+func LatencyMiddleware(c *gin.Context) {
+	start := time.Now()
 
 	// Pass on to the next-in-chain
 	c.Next()
 
-	fmt.Println("Out of dummy")
+	elapsed := time.Since(start).Microseconds()
+	logger.Log(fmt.Sprintf("Elapsed time in micro seconds: [%d]", elapsed))
 }
 
 // StartApplication starts the application
 func StartApplication() {
 	logger.Log("Starting application...")
 
-	router.Use(DummyMiddleware)
+	p := ginprometheus.NewPrometheus("gin")
+	p.Use(router)
+
+	router.Use(LatencyMiddleware)
 
 	mapUrls()
 	router.Run(":8080")
